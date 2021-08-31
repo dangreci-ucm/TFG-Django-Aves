e18a46fd202342c4e5e1cea6acd620498610473c
Enolah <paulsa07@ucm.es>
2021-09-01 01:40:55 +0200

    interfaz de identificacion

--- apptfg/views.py
+++ apptfg/views.py
@@ -24,9 +24,9 @@ def contacto(request):
    return render (request,'apptfg/contacto.html',{})
 
 def calcular(request):
-   print("CALCULANDO")
+   
    if request.method=='POST':
-      print("method post")
+   
       todos_huesos = {
                      'coxalL':request.POST.get('coxalL'), 
                      'coxalA':request.POST.get('coxalA'),
@@ -42,19 +42,19 @@ def calcular(request):
                      }
                            
       # eliminamos los que tengan valor 0
-      print(todos_huesos.items())
+      
       huesos = {k:float(v) for k,v in todos_huesos.items() if v!=''}
       if len(huesos)<=0: 
          return render(request,'apptfg/identificacion.html', {'msg':'Debe introducir al menos un valor'})
 
       result, score_modelo = prediction.main(data_from_excel.data,SMOTE(), huesos)
-      print(result)
+      
       # Calculate percentage
       result=percentage(result)
       # Sort the result of the prediction, result is a dictionarity
       result_sort= sorted(result.items(), key=operator.itemgetter(1),reverse=True) 
-      print(result_sort)                     
-      return render(request,'apptfg/identificacion.html', {'msg':'Resultados:','valor': result_sort, 'ave':result_sort[0]})
+
+      return render(request,'apptfg/identificacion.html', {'msg':'Resultados','valor': result_sort})
 
    return render(request,'apptfg/identificacion.html',{})
 
